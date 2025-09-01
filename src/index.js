c652c3becc28c7726d359370590dccf96ff5221d
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-09-01 17:33:09 +0300

    feat: setup db connection and users route/controller structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { pool } from "./db/pool.js";
 
 const app = express();
 const PORT = 5001;
@@ -11,6 +12,17 @@ app.use(express.json());
 //test route
 app.get("/ping", (req, res) => {
   res.send("server is alive!");
+});
+
+//db test route
+app.get("/db-check", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({ dbConnected: true, time: result.rows[0] });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ dbConnected: false, error: error.message });
+  }
 });
 
 //start server
