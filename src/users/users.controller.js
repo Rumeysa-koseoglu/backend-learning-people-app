a9f4f46059c44654a6afa466401bfe071ac53461
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-09-05 21:29:41 +0300

    feat: add POST /users endpoint to create new user

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,6 +1,6 @@
 // Controller = Layer of "work logic" + "connection with DB"
 // Route knows "which address", and Controller knows "what we going to do"
-import { Pool } from "./db/pool.js";
+import { pool } from "./db/pool.js";
 
 //GET /users -> get all users
 // this function is async because it requires a round trip to the DB
@@ -12,8 +12,21 @@ export const getAllUsers = async (req, res, next) => {
     //result.rows is an array; we return the lines as json
     // The browser/client can parse this directly and list it on the screen
     res.json(result.rows);
-  } catch (error) {
+  } catch (err) {
     //if an error occurs in the try, send it to the express error handler (error middleware)
+    next(err);
+  }
+};
+
+export const createUser = async (req, res, next) => {
+  try {
+    const { name, age } = req.body;
+
+    const result = await pool.query(
+      "INSERT INTO users (name, age) VALUES ($1, $2) RETURNING *"[(name, age)]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
     next(err);
   }
 };
