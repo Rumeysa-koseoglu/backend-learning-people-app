08b93f7a657e469132d530b2a344595455d43ac4
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-09-14 15:11:48 +0300

    feat(client): add user creation form, display users and integrate with backend API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "../src/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+  //when the component is loaded, pull the users from the backend
+  useEffect(() => {
+    fetch("http://localhost:5001/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  //function to add a new user (will be called when the form is submitted)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newUser = { name, age: Number(age) };
+
+    const res = await fetch("http://localhost:5001/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newUser),
+    });
+
+    const data = await res.json();
+
+    //add the added user to the list
+    setUsers([...users, data]);
+
+    //clean the form
+    setName("");
+    setAge("");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1>USERS</h1>
+
+      {/* new user addition form */}
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          className="name-input"
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          className="age-input"
+          type="number"
+          placeholder="age"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <button type="submit">Add User</button>
+      </form>
+
+      <ul>
+        {users.map((u) => (
+          <li key={u.id}>
+            {u.name} ({u.age})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
